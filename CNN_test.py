50909088e5fe66cd2f917820f0d80fba9bf412d2
XBG <xabier.blanch@tu-dresden.de>
2021-08-31 16:12:42 +0200

    31.08.21

--- CNN_test.py
+++ CNN_test.py
@@ -3,21 +3,31 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, UpSamp
 
 def CNN_test(IMG_SIZE):
     model = Sequential()
-    model.add(Conv2D(6,(3,3), padding='same', activation='relu', input_shape=(IMG_SIZE, IMG_SIZE, 3)))
-    model.add(Conv2D(6, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(64,(3,3), padding='same', activation='relu', input_shape=(IMG_SIZE, IMG_SIZE, 3)))
+    model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
     model.add(MaxPooling2D((2,2), strides=(2,2)))
     
-    model.add(Conv2D(9,(3,3), padding='same', activation='relu'))
-    model.add(Conv2D(9, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(128,(3,3), padding='same', activation='relu'))
+    model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+    model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
     model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
     model.add(UpSampling2D((2, 2)))
-    model.add(Conv2D(9, (3, 3), padding='same', activation='relu'))
-    model.add(Conv2D(9, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
 
     model.add(UpSampling2D((2, 2)))
-    model.add(Conv2D(6, (3, 3), padding='same', activation='relu'))
-    model.add(Conv2D(6, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
+
+    model.add(UpSampling2D((2, 2)))
+    model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
     
     model.add(Dense(1,activation='softmax'))
     model.summary()
