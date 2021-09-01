17efd09d1d6c81f89e3fe3e96a3ad16e59b8968e
XBG <xabier.blanch@tu-dresden.de>
2021-09-01 11:10:24 +0200

    01.09.2021

--- main.py
+++ main.py
@@ -13,33 +13,23 @@ DATADIR = 'C:/Users/XBG-KIWA/Documents/01_GITHUB_(XBG)/SegNet/data'
 IMG_SIZE = 128
 
 #load images and masks -> resize and normalise
-train_images, train_masks, training_data = create_training_data(DATADIR, IMG_SIZE)
+train_images, train_masks, val_images, val_masks = create_training_data(DATADIR, IMG_SIZE)
 
 #chech images
-show_images(25, training_data)
+# show_images(25, training_data)
 
 #load CNN SegNet
 model = SegNet(IMG_SIZE,IMG_SIZE)
 model.summary()
-model.compile(optimizer='sgd', loss = tf.keras.losses.BinaryCrossentropy(from_logits=True), metrics=['accuracy'])
-history = model.fit(train_images, train_masks, batch_size=1 ,epochs=5)
+opt = SGD(lr=0.001, momentum=0.9, decay=0.0005)
+model.compile(optimizer=opt, loss = tf.keras.losses.BinaryCrossentropy(from_logits=True), metrics=['accuracy'])
+history = model.fit(train_images, train_masks, batch_size=5 ,epochs=5)
 
-pred_mask = model.predict(train_images)
 
-
-#load CNN SegNet
-# model=CNN_test(IMG_SIZE)
-# model.summary()
-# # opt = SGD(lr=0.001, momentum=0.9, decay=0.0005)
-# model.compile(loss='binary_crossentropy', optimizer='sgd', metrics=['accuracy'])
-# history = model.fit(train_images, train_masks, batch_size=1 ,epochs=5)
-#
 # pred_mask = model.predict(train_images)
-
-
-plt.imshow(train_images[0])
-plt.imshow(pred_mask[0], alpha=0.35)
-plt.show()
+# plt.imshow(train_images[0])
+# plt.imshow(pred_mask[0], alpha=0.35)
+# plt.show()
 
 
 
