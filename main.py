c8e2b172ae002ceb38511d4818de1eff7b472ec8
XBG <xabier.blanch@tu-dresden.de>
2021-09-01 13:22:25 +0200

    01.09.2021

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.optimizers import SGD
 from tensorflow.keras.preprocessing.image import *
+from keras.models import load_model
 from SegNet import *
 from CNN_test import *
 from utils import *
 
 print('Tensorflow version: ', tf.__version__)
 print('Keras version: ', keras.__version__)
+
+# Load the storage model
+# model = load_model('models/SegNet_t1.h5')
 
 #main_parameters
 TRAIN_DIR = 'C:/Users/XBG-KIWA/Documents/01_GITHUB_(XBG)/SegNet/data/train'
@@ -28,17 +32,16 @@ model = SegNet(IMG_SIZE,IMG_SIZE)
 model.summary()
 opt = SGD(lr=0.001, momentum=0.9, decay=0.0005)
 model.compile(optimizer=opt, loss = tf.keras.losses.BinaryCrossentropy(from_logits=True), metrics=['accuracy'])
-history = model.fit(train_images, train_masks, batch_size=5 ,epochs=5)
+history = model.fit(train_images, train_masks, batch_size=4 ,epochs=10)
+test_loss, test_acc = model.evaluate(val_images, val_masks, batch_size=1)
+test_masks = model.predict(train_images)
 
-test_loss, test_acc = model.evaluate(train_images, train_masks, batch_size=1)
-
-pred_mask = model.predict(train_images)
-
-plt.imshow(train_masks[43])
-plt.imshow(pred_mask[43], alpha=0.35)
+plt.imshow(train_images[5])
+plt.imshow(train_masks[5], alpha=0.35)
+plt.show()
+plt.imshow(train_images[5])
+plt.imshow(test_masks[5], alpha=0.35)
 plt.show()
 
-
-
-TRAIN_DIR = 'C:/Users/XBG-KIWA/Documents/01_GITHUB_(XBG)/SegNet/data'
-VALIDATION_DIR = './datasets/validation'
+# Save the trained model as hdf5 file
+model.save('models/SegNet_t1.h5')
--- utils.py
+++ utils.py
@@ -8,29 +8,28 @@ def create_datasets(DIR, IMG_SIZE):
   masks = []
   path = os.path.join(DIR, 'img')
   for img in os.listdir(path):
-    if not os.listdir(path):
-      print('Directory ' + DIR + '/img is empty')
-      continue
-    else:
-      img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_COLOR)
-      img_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
-      img_array = cv2.normalize(img_array, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
-      images.append([img_array])
-
-    if not os.listdir(os.path.join(DIR, 'label', img)):
-      print('Directory ' + DIR + '/label is empty')
-      continue
-    else:
+    img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_COLOR)
+    img_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+    img_array = cv2.normalize(img_array, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+    images.append([img_array])
+    try:
       mask_array = cv2.imread(os.path.join(DIR, 'label', img), cv2.IMREAD_GRAYSCALE)
       mask_array = cv2.resize(mask_array, (IMG_SIZE, IMG_SIZE))
       mask_array = cv2.normalize(mask_array, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX,
-                                        dtype=cv2.CV_32F)
+                                          dtype=cv2.CV_32F)
       masks.append([mask_array])
-
-  train_images = np.array(images)
-  train_images = train_images.reshape(len(os.listdir(os.path.join(DIR, 'img'))), IMG_SIZE, IMG_SIZE, 3)
-  train_masks = np.array(masks)
-  train_masks = train_masks.reshape(len(os.listdir(os.path.join(DIR, 'label'))), IMG_SIZE, IMG_SIZE, 1)
+    except:
+      continue
+  if images:
+    train_images = np.array(images)
+    train_images = train_images.reshape(len(os.listdir(os.path.join(DIR, 'img'))), IMG_SIZE, IMG_SIZE, 3)
+  else:
+    train_images = []
+  if masks:
+    train_masks = np.array(masks)
+    train_masks = train_masks.reshape(len(os.listdir(os.path.join(DIR, 'label'))), IMG_SIZE, IMG_SIZE, 1)
+  else:
+    train_masks = []
   return train_images, train_masks
 
 def show_images(id, data):
